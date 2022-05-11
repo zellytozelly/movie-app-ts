766176983f30429488dbb19c87f8d481c1c286f7
zellytozelly <zellytozelly@gmail.com>
2022-05-11 11:30:16 +0900

    Feat: Add search button step1

--- src/routes/Movie/index.tsx
+++ src/routes/Movie/index.tsx
@@ -3,18 +3,39 @@ import styles from './Movie.module.scss'
 import MovieList from './MoviePage/MovieList'
 import MovieFavorite from './MoviePage/MovieFavorite'
 import { SearchIcon } from 'assets/svgs'
+import { KeyboardEvent, ChangeEvent, useState } from 'react'
 
 const Movie = () => {
   const { pageSection } = useParams<{ pageSection: string }>()
+  const [ searchValue, setSearchValue] = useState('')
+
+  const handleSearchInputChange = (event: ChangeEvent<HTMLInputElement>): void => {
+    setSearchValue(event.currentTarget.value)
+    // setPage(Number(value) + 1)
+  }
+
+  const handleSearchInputKeyDown = (event: KeyboardEvent<HTMLInputElement>): void => {
+    if (event.key === 'Enter') {
+      console.log(searchValue)
+    }
+    // setPage(Number(value) + 1)
+  }
+
 
   return (
     <section className={styles.movieWrap}>
       
       <header className={styles.movieHeader}>
         <div className={styles.searchBox}>
-          <input type='text' className={styles.searchInput} placeholder='영화 검색'/>
+          <input 
+            type='text' 
+            className={styles.searchInput} 
+            placeholder='영화 검색'
+            onChange={handleSearchInputChange}
+            onKeyDown={handleSearchInputKeyDown}
+          />
           <div className={styles.searchIcon}><SearchIcon /></div>
-          <button type='button' aria-label='Search button' />    
+          <button type='button' aria-label='Search button' className={styles.searchButton}>GO</button>    
         </div>
       </header>
 
